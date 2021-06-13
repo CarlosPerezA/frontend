c9f8cdb445264e1d8a787a6713b9b3aa3e803a46
Carlos Perez <carlos@Carloss-MacBook-Pro>
2021-06-13 06:02:26 -0500

    proyecto

---
+++ src/routes/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Initial from "../containers/Initial";
+import Register from '../containers/Register';
+import Profile from "../containers/Profile";
+import Verify from "../containers/Verify";
+
+
+const App = () => (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Initial} />
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/profile" component={Profile} />
+        <Route exact path="/verify" component={Verify} />
+      </Switch>
+    </BrowserRouter>
+);
+
+export default App;
